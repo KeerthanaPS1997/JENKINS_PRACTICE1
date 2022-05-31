d61dc98f4a722d47d2d9727bd4da6f1268d9c285
Arkesh <Arkesh@LAPTOP-OQBK3O27>
2022-05-31 12:22:40 +0530

    This is my first push

--- src/main/java/com/SDET34L1/genericUtility/BaseClass.java
+++ src/main/java/com/SDET34L1/genericUtility/BaseClass.java
@@ -21,6 +21,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 /**
  * This class contains implementation of all basic configuration annotations
  * @author Arkesh
+ * Base class is updated
  *
  */
 public class BaseClass 
